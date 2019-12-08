669b97ab91fe9df683392e36d2f654c97370e005
David F. Severski <davidski@deadheaven.com>
2019-12-08 07:07:22 -0800

    Output the cloudfront zone id.

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "arn" {
 output "domain_name" {
   value = aws_cloudfront_distribution.ssl_distribution.domain_name
 }
+
+output "hosted_zone_id" {
+  value = aws_cloudfront_distribution.ssl_distribution.hosted_zone_id
+}
+
